5806d325afe3c931e95794a16279d22b9dbb2e00
alamin <alamin@gmail.com>
2021-02-28 23:21:23 +0600

    updated

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h2>This is a about page</h2>
+    </div>
+  );
+};
+
+export default about;
